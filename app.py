1947047f8a105fb0898c8467cc968fe8b8136b3d
matteolucahomsy <matteoluca.homsy@net.usj.edu.lb>
2026-05-18 15:17:41 +0300

    lvl4+: put add button

--- app.py
+++ app.py
@@ -9,5 +9,20 @@ manager = StudentManager()
 def home():
     students=manager.get_all_students()
     return render_template("index.html", students=students)
+@app.route("/add", methods=["GET", "POST"])
+def add_student():
+    if request.method == "POST":
+        student_id =int (request.form.get("id"))
+        name=request.form.get("name")
+        age=int(request.form.get("age"))
+        grade=float(request.form.get("grade"))
+
+        student=Student(student_id,name,age,grade)
+        manager.add_student(student)
+        return redirect("/")
+    return render_template("add.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
