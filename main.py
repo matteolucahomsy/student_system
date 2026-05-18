1fe0449a67440c5663007a75f6278bf044b2990d
matteolucahomsy <matteoluca.homsy@net.usj.edu.lb>
2026-05-18 14:32:54 +0300

    level3: add statistics , ranking improvements,CSV exports, graphs

--- main.py
+++ main.py
@@ -5,35 +5,69 @@ manager=StudentManager()
 
 while True:
     print("""
+=========================
+Student Management System
+=========================
 1. Add student 
 2. Delete student
 3. Search student
 4. Update grade
 5. Display students 
-6. Exit
+6. Class average
+7. Best student
+8. Ranking
+9. Export CSV
+10. Show graph
+11. Exit
 """)
     choice =input("Enter choice: ")
     if choice =="1":
-        student_id=int(input("ID: "))
-        name=input("Name: ")
-        age=int(input("Age: "))
-        grade=float(input("Grade: "))
-
+        try:
+            student_id=int(input("ID: "))
+            name=input("Name: ")
+            age=int(input("Age: "))
+            grade=float(input("Grade: "))
+        except ValueError:
+            print("Invalid input!")
+            continue
         student= Student(student_id,name,age,grade)
         manager.add_student(student)
     elif choice == "2":
-        student_id =int(input("ID to delte: "))
+        try:
+            student_id =int(input("ID to delete: "))
+        except ValueError:
+            print("Invalid ID")
+            continue
         manager.delete_student(student_id)
     elif choice == "3":
-        student_id=int(input("ID: "))
+        try:
+            student_id=int(input("ID: "))
+        except ValueError:
+            print("Invalid ID!")
+            continue
         manager.search_student(student_id)
     elif choice == "4":
-        student_id=int(input("ID: "))
-        new_grade=float(input("New grade: "))
+        try:
+            student_id=int(input("ID: "))
+            new_grade=float(input("New grade: "))
+        except ValueError:
+            print("Invalid Input!")
+            continue
         manager.update_grade(student_id,new_grade)
     elif choice=="5":
         manager.display_students()
     elif choice == "6":
+        manager.class_average()
+    elif choice == "7":
+        manager.best_student()
+    elif choice == "8":
+        manager.ranking()
+    elif choice == "9":
+        manager.export_csv()
+    elif choice == "10":
+        manager.plot_grades()
+    elif choice == "11":
+        print("BYE")
         break
     else:
         print("Invalid choice")
--- manager.py
+++ manager.py
@@ -1,3 +1,5 @@
+import csv 
+import matplotlib.pyplot as plt
 import sqlite3
 from student import Student
 import os 
@@ -53,12 +55,69 @@ class StudentManager:
         conn.close()
 
         print(student)
+    def class_average(self):
+        conn=sqlite3.connect("students.db")
+        cursor=conn.cursor()
+        cursor.execute("SELECT AVG(grade) FROM students")
+        avg=cursor.fetchone()[0]
+        conn.close()
+        print("Class average: ",avg)
+    def best_student(self):
+        conn=sqlite3.connect("students.db")
+        cursor=conn.cursor()
+        cursor.execute("SELECT MAX(grade) FROM students")
+        max_grade=cursor.fetchone()[0]
+        cursor.execute("SELECT * FROM students WHERE grade = ?", (max_grade,))
+        students=cursor.fetchall()
+        conn.close()
+        print("Best students: ")
+        for s in students:
+            print(s)
+    def ranking(self):
+        conn=sqlite3.connect("students.db")
+        cursor=conn.cursor()
+        cursor.execute("SELECT * FROM students  ORDER BY grade DESC")
+        data=cursor.fetchall()
+        conn.close()
+        last_grade=Nonerank=0
+        real_rank=0
+        for s in data:
+            real_rank+=1
+            if s[3]!= last_grade:
+                rank= real_rank
+                last_grade=s[3]
+            print(f"{rank}. {s}")
+
+    def export_csv(self):
+        conn=sqlite3.connect("students.db")
+        cursor=conn.cursor()
+        cursor.execute("SELECT * FROM students")
+        data=cursor.fetchall()
+        conn.close()
+        with open("students.csv","w",newline="") as file:
+            writer=csv.writer(file)
+            writer.writerow(["ID","Name","Age", "grade"])
+            writer.writerows(data)
+        print("Export done!")
+    def plot_grades(self):
+        conn=sqlite3.connect("students.db")
+        cursor=conn.cursor()
+        cursor.execute("SELECT name, grade FROM students")
+        data=cursor.fetchall()
+        conn.close()
+        if not data:
+            print("No students found")
+            return
+        names=[x[0] for x in data]
+        grades=[x[1] for x in data]
+        plt.bar(names, grades)
+        plt.title("Student Grades")
+        plt.show()
     def display_students(self):
         conn=sqlite3.connect("students.db")
         cursor=conn.cursor()
         cursor.execute("SELECT * FROM students")
         students= cursor.fetchall()
-
         for s in students:
             print(s)
         conn.close()
