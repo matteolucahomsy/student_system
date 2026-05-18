22a0b38dc806b480622278ecf6dd7bd325608ab3
matteolucahomsy <matteoluca.homsy@net.usj.edu.lb>
2026-05-18 13:27:43 +0300

    add of more sql functions

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ manager=StudentManager()
 while True:
     print("""
 1. Add student 
-2. Display students 
-3. Exit
+2. Delete student
+3. Search student
+4. Update grade
+5. Display students 
+6. Exit
 """)
     choice =input("Enter choice: ")
     if choice =="1":
@@ -18,9 +21,19 @@ while True:
 
         student= Student(student_id,name,age,grade)
         manager.add_student(student)
-    elif choice=="2":
-        manager.display_students()
+    elif choice == "2":
+        student_id =int(input("ID to delte: "))
+        manager.delete_student(student_id)
     elif choice == "3":
+        student_id=int(input("ID: "))
+        manager.search_student(student_id)
+    elif choice == "4":
+        student_id=int(input("ID: "))
+        new_grade=float(input("New grade: "))
+        manager.update_grade(student_id,new_grade)
+    elif choice=="5":
+        manager.display_students()
+    elif choice == "6":
         break
     else:
         print("Invalid choice")
--- manager.py
+++ manager.py
@@ -21,6 +21,38 @@ class StudentManager:
         conn.close()
 
         print("Student added seccessfully!")
+    def delete_student(self,student_id):
+        conn=sqlite3.connect("students.db")
+        cursor=conn.cursor()
+
+        cursor.execute("DELETE FROM students WHERE id = ?")
+        
+        conn.commit()
+        conn.close()
+
+        print("Student deleted!")
+    def update_grade(self,student_id,new_grade):
+        conn=sqlite3.connect("students.db")
+        cursor=conn.cursor()
+
+        cursor.execute("""
+        UPDATE  students
+        SET grade = ?
+        WHERE id = ?
+        """,(new_grade,student_id))
+        conn.commit()
+        conn.close()
+
+        print("Grade updated!")
+    def search_student(self,student_id):
+        conn=sqlite3.connect("students.db")
+        cursor=conn.cursor()
+
+        cursor.execute("SELECT * FROM students WHERE id = ?",(student_id))
+        student=cursor.fetchone()
+        conn.close()
+
+        print(student)
     def display_students(self):
         conn=sqlite3.connect("students.db")
         cursor=conn.cursor()
