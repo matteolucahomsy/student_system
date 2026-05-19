3ebdb2d493faea0f48f67066e3a8c2494c1a8f29
matteolucahomsy <matteoluca.homsy@net.usj.edu.lb>
2026-05-19 09:50:27 +0300

    add.py modified

--- app.py
+++ app.py
@@ -12,14 +12,20 @@ def home():
 @app.route("/add", methods=["GET", "POST"])
 def add_student():
     if request.method == "POST":
-        student_id =int (request.form.get("id"))
-        name=request.form.get("name")
-        age=int(request.form.get("age"))
-        grade=float(request.form.get("grade"))
-
-        student=Student(student_id,name,age,grade)
-        manager.add_student(student)
-        return redirect("/")
+        try:
+            student_id =int (request.form.get("id"))
+            name=request.form.get("name")
+            age=int(request.form.get("age"))
+            if age <=0:
+                return "Age must be positive"
+            grade=float(request.form.get("grade"))
+            if grade<0 or grade>20:
+                return "Grade must be between 0 and 20"
+            student=Student(student_id,name,age,grade)
+            manager.add_student(student)
+            return redirect("/")
+        except ValueError:
+            return "Invalid input"
     return render_template("add.html")
 @app.route("/delete/<int:student_id>")
 def delete_student(student_id):
@@ -29,6 +35,8 @@ def delete_student(student_id):
 def update_student(student_id):
     if request.method=="POST":
         new_grade=float(request.form.get("grade"))
+        if new_grade<0 or new_grade>20:
+            return "New grade must be between 0 and 20"
         manager.update_grade(student_id, new_grade)
         return redirect("/")
     return render_template("update.html", student_id=student_id)
